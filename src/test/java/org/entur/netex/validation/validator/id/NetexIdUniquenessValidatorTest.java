1a98ba18b62a9a6f808dabf977296c7f075782c6
vincent <vincent.paturet@entur.org>
2022-01-17 15:21:41 +0100

    Add unit tests

--- src/test/java/org/entur/netex/validation/validator/id/NetexIdUniquenessValidatorTest.java
+++ src/test/java/org/entur/netex/validation/validator/id/NetexIdUniquenessValidatorTest.java
@@ -15,24 +15,21 @@ class NetexIdUniquenessValidatorTest {
     private static final String TEST_DATASET_CODESPACE = "FLB";
     public static final String TEST_REPORT_ID = "report-id-test";
 
-
     @Test
     void testNoDuplicate() throws IOException {
-
-        NetexIdRepository netexIdRepository = new DefaultNetexIdRepository();
-        NetexIdUniquenessValidator netexIdUniquenessValidator = new NetexIdUniquenessValidator(netexIdRepository);
-
+        NetexIdUniquenessValidator netexIdUniquenessValidator = createValidator();
         ValidationReport aggregatedValidationReport = getReport(TEST_DATASET_CODESPACE, TEST_REPORT_ID, TEST_DATASET_FILE_NAME, netexIdUniquenessValidator);
         Assertions.assertTrue(aggregatedValidationReport.getValidationReportEntries().isEmpty());
     }
 
     @Test
     void testDuplicate() throws IOException {
-
-        NetexIdRepository netexIdRepository = new DefaultNetexIdRepository();
-        NetexIdUniquenessValidator netexIdUniquenessValidator = new NetexIdUniquenessValidator(netexIdRepository);
-
+        NetexIdUniquenessValidator netexIdUniquenessValidator = createValidator();
         ValidationReport aggregatedValidationReport = getReport(TEST_DATASET_CODESPACE, TEST_REPORT_ID, TEST_DATASET_DUPLICATE_ID_FILE_NAME, netexIdUniquenessValidator);
         Assertions.assertFalse(aggregatedValidationReport.getValidationReportEntries().isEmpty());
+    }
+
+    private NetexIdUniquenessValidator createValidator() {
+        return new NetexIdUniquenessValidator(new DefaultNetexIdRepository());
     }
 }
