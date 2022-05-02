48f632cc0c8f10ffa1691b21f386ebd15d72805a
vincent <vincent.paturet@entur.org>
2022-05-02 11:56:04 +0200

    Add validation progress callback

---
+++ src/main/java/org/entur/netex/validation/validator/NoopNetexValidationCallBack.java
@@ -0,0 +1,12 @@
+package org.entur.netex.validation.validator;
+
+/**
+ * No-op Validation callback.
+ */
+public class NoopNetexValidationCallBack implements NetexValidationProgressCallBack {
+
+    @Override
+    public void notifyProgress(String message) {
+        // NOOP
+    }
+}
