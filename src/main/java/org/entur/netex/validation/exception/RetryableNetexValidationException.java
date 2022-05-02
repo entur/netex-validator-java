7f4fe0e9bf45924c610760f6493cd0c29abcbab9
vpaturet <46598384+vpaturet@users.noreply.github.com>
2022-05-02 14:50:30 +0200

    Run async schema validation (#47)

---
+++ src/main/java/org/entur/netex/validation/exception/RetryableNetexValidationException.java
@@ -0,0 +1,7 @@
+package org.entur.netex.validation.exception;
+
+public class RetryableNetexValidationException extends NetexValidationException {
+    public RetryableNetexValidationException(Throwable t) {
+        super(t);
+    }
+}
