2546ad7126d8f7b153586075a636d03bdaabafd2
vincent <vincent.paturet@entur.org>
2022-05-02 13:46:07 +0200

    Run async schema validation

--- src/main/java/org/entur/netex/validation/exception/RetryableNetexValidationException.java
+++ src/main/java/org/entur/netex/validation/exception/RetryableNetexValidationException.java
@@ -1,7 +1,5 @@
 package org.entur.netex.validation.exception;
 
-import org.entur.netex.validation.exception.NetexValidationException;
-
 public class RetryableNetexValidationException extends NetexValidationException {
     public RetryableNetexValidationException(Throwable t) {
         super(t);
