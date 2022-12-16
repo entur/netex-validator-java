0b0f83d0ebc2debe2a454739134358352f9712eb
vincent <vincent.paturet@entur.org>
2022-12-16 17:06:31 +0100

    Update flexible line validation rules

--- src/main/java/org/entur/netex/validation/validator/xpath/rules/ValidateAllowedBookingWhenProperty.java
+++ src/main/java/org/entur/netex/validation/validator/xpath/rules/ValidateAllowedBookingWhenProperty.java
@@ -8,10 +8,8 @@ import org.rutebanken.netex.model.PurchaseWhenEnumeration;
 public class ValidateAllowedBookingWhenProperty extends ValidateNotExist {
 
     private static final String VALID_BOOKING_WHEN_PROPERTIES = "'" + String.join("','",
-            PurchaseWhenEnumeration.TIME_OF_TRAVEL_ONLY.value(),
             PurchaseWhenEnumeration.DAY_OF_TRAVEL_ONLY.value(),
             PurchaseWhenEnumeration.UNTIL_PREVIOUS_DAY.value(),
-            PurchaseWhenEnumeration.ADVANCE_ONLY.value(),
             PurchaseWhenEnumeration.ADVANCE_AND_DAY_OF_TRAVEL.value())
             + "'";
 
