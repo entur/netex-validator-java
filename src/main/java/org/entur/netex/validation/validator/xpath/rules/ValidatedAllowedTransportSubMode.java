5d0f1fcbb994c1ad6a73f7df2ccfc2e9f37566e6
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-11-06 22:13:45 +0100

    Reformat

--- src/main/java/org/entur/netex/validation/validator/xpath/rules/ValidatedAllowedTransportSubMode.java
+++ src/main/java/org/entur/netex/validation/validator/xpath/rules/ValidatedAllowedTransportSubMode.java
@@ -16,77 +16,73 @@ import static org.rutebanken.netex.model.WaterSubmodeEnumeration.*;
  */
 public class ValidatedAllowedTransportSubMode extends ValidateNotExist {
 
-    private static final String VALID_TRANSPORT_SUBMODES = "'" + String.join("','",
+    private static final String VALID_TRANSPORT_SUBMODES =
+            "'" +
+                    String.join(
+                            "','",
+                            // Coach
+                            INTERNATIONAL_COACH.value(),
+                            NATIONAL_COACH.value(),
+                            TOURIST_COACH.value(),
+                            // Tram
+                            LOCAL_TRAM.value(),
+                            CITY_TRAM.value(),
+                            // Bus
+                            AIRPORT_LINK_BUS.value(),
+                            EXPRESS_BUS.value(),
+                            LOCAL_BUS.value(),
+                            NIGHT_BUS.value(),
+                            RAIL_REPLACEMENT_BUS.value(),
+                            REGIONAL_BUS.value(),
+                            SCHOOL_BUS.value(),
+                            SIGHTSEEING_BUS.value(),
+                            SHUTTLE_BUS.value(),
+                            // Metro
+                            METRO.value(),
+                            // Water
+                            HIGH_SPEED_PASSENGER_SERVICE.value(),
+                            HIGH_SPEED_VEHICLE_SERVICE.value(),
+                            // Telecabin
+                            TELECABIN.value(),
+                            // Funicular
+                            FUNICULAR.value(),
+                            // Rail
+                            INTERNATIONAL.value(),
+                            INTERREGIONAL_RAIL.value(),
+                            LOCAL.value(),
+                            LONG_DISTANCE.value(),
+                            NIGHT_RAIL.value(),
+                            AIRPORT_LINK_RAIL.value(),
+                            REGIONAL_RAIL.value(),
+                            TOURIST_RAILWAY.value(),
+                            // Air
+                            HELICOPTER_SERVICE.value(),
+                            DOMESTIC_FLIGHT.value(),
+                            INTERNATIONAL_FLIGHT.value(),
+                            // Water
+                            INTERNATIONAL_CAR_FERRY.value(),
+                            INTERNATIONAL_PASSENGER_FERRY.value(),
+                            LOCAL_CAR_FERRY.value(),
+                            LOCAL_PASSENGER_FERRY.value(),
+                            NATIONAL_CAR_FERRY.value(),
+                            SIGHTSEEING_SERVICE.value(),
+                            // Taxi
+                            CHARTER_TAXI.value(),
+                            COMMUNAL_TAXI.value(),
+                            // All
+                            "unknown"
+                    ) +
+                    "'";
 
-            // Coach
-            INTERNATIONAL_COACH.value(),
-            NATIONAL_COACH.value(),
-            TOURIST_COACH.value(),
-
-            // Tram
-            LOCAL_TRAM.value(),
-            CITY_TRAM.value(),
-
-            // Bus
-            AIRPORT_LINK_BUS.value(),
-            EXPRESS_BUS.value(),
-            LOCAL_BUS.value(),
-            NIGHT_BUS.value(),
-            RAIL_REPLACEMENT_BUS.value(),
-            REGIONAL_BUS.value(),
-            SCHOOL_BUS.value(),
-            SIGHTSEEING_BUS.value(),
-            SHUTTLE_BUS.value(),
-
-            // Metro
-            METRO.value(),
-
-            // Water
-            HIGH_SPEED_PASSENGER_SERVICE.value(),
-            HIGH_SPEED_VEHICLE_SERVICE.value(),
-
-            // Telecabin
-            TELECABIN.value(),
-
-            // Funicular
-            FUNICULAR.value(),
-
-            // Rail
-            INTERNATIONAL.value(),
-            INTERREGIONAL_RAIL.value(),
-            LOCAL.value(),
-            LONG_DISTANCE.value(),
-            NIGHT_RAIL.value(),
-            AIRPORT_LINK_RAIL.value(),
-            REGIONAL_RAIL.value(),
-            TOURIST_RAILWAY.value(),
-
-            // Air
-            HELICOPTER_SERVICE.value(),
-            DOMESTIC_FLIGHT.value(),
-            INTERNATIONAL_FLIGHT.value(),
-
-            // Water
-            INTERNATIONAL_CAR_FERRY.value(),
-            INTERNATIONAL_PASSENGER_FERRY.value(),
-            LOCAL_CAR_FERRY.value(),
-            LOCAL_PASSENGER_FERRY.value(),
-            NATIONAL_CAR_FERRY.value(),
-            SIGHTSEEING_SERVICE.value(),
-
-            // Taxi
-            CHARTER_TAXI.value(),
-            COMMUNAL_TAXI.value(),
-
-            // All
-            "unknown")
-            + "'";
-
-  private static final String MESSAGE = "Illegal TransportSubMode";
+    private static final String MESSAGE = "Illegal TransportSubMode";
 
     public ValidatedAllowedTransportSubMode() {
-        super("lines/*[self::Line or self::FlexibleLine]/TransportSubmode[not(. = (" + VALID_TRANSPORT_SUBMODES + "))]",
+        super(
+                "lines/*[self::Line or self::FlexibleLine]/TransportSubmode[not(. = (" +
+                        VALID_TRANSPORT_SUBMODES +
+                        "))]",
                 MESSAGE,
-                "TRANSPORT_SUB_MODE");
+                "TRANSPORT_SUB_MODE"
+        );
     }
 }
