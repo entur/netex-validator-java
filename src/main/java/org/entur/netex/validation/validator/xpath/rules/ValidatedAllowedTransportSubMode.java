1852014259afcb119da0ddc5b059f3713d477f57
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-11-06 17:12:52 +0100

    Added the taxi sub modes for validation.

--- src/main/java/org/entur/netex/validation/validator/xpath/rules/ValidatedAllowedTransportSubMode.java
+++ src/main/java/org/entur/netex/validation/validator/xpath/rules/ValidatedAllowedTransportSubMode.java
@@ -1,63 +1,92 @@
 package org.entur.netex.validation.validator.xpath.rules;
 
+import static org.rutebanken.netex.model.AirSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.BusSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.CoachSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.FunicularSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.MetroSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.RailSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.TaxiSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.TelecabinSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.TramSubmodeEnumeration.*;
+import static org.rutebanken.netex.model.WaterSubmodeEnumeration.*;
+
 /**
  * Validate the transport sub-mode against the Nordic NeTEx profile.
  */
 public class ValidatedAllowedTransportSubMode extends ValidateNotExist {
 
-  private static final String VALID_TRANSPORT_SUBMODES =
-    "'" +
-    String.join(
-      "','",
-      "internationalCoach",
-      "nationalCoach",
-      "touristCoach",
-      "airportLinkBus",
-      "localTram",
-      "cityTram",
-      "international",
-      "metro",
-      "domesticFlight",
-      "highSpeedPassengerService",
-      "telecabin",
-      "funicular",
-      "expressBus",
-      "interregionalRail",
-      "helicopterService",
-      "highSpeedVehicleService",
-      "localBus",
-      "local",
-      "internationalFlight",
-      "internationalCarFerry",
-      "nightBus",
-      "longDistance",
-      "internationalPassengerFerry",
-      "railReplacementBus",
-      "nightRail",
-      "airportLinkRail",
-      "localCarFerry",
-      "regionalBus",
-      "regionalRail",
-      "localPassengerFerry",
-      "schoolBus",
-      "touristRailway",
-      "nationalCarFerry",
-      "shuttleBus",
-      "sightseeingService",
-      "sightseeingBus",
-      "unknown"
-    ) +
-    "'";
+    private static final String VALID_TRANSPORT_SUBMODES = "'" + String.join("','",
+
+            // Coach
+            INTERNATIONAL_COACH.value(),
+            NATIONAL_COACH.value(),
+            TOURIST_COACH.value(),
+
+            // Tram
+            LOCAL_TRAM.value(),
+            CITY_TRAM.value(),
+
+            // Bus
+            AIRPORT_LINK_BUS.value(),
+            EXPRESS_BUS.value(),
+            LOCAL_BUS.value(),
+            NIGHT_BUS.value(),
+            RAIL_REPLACEMENT_BUS.value(),
+            REGIONAL_BUS.value(),
+            SCHOOL_BUS.value(),
+            SIGHTSEEING_BUS.value(),
+            SHUTTLE_BUS.value(),
+
+            // Metro
+            METRO.value(),
+
+            // Water
+            HIGH_SPEED_PASSENGER_SERVICE.value(),
+            HIGH_SPEED_VEHICLE_SERVICE.value(),
+
+            // Telecabin
+            TELECABIN.value(),
+
+            // Funicular
+            FUNICULAR.value(),
+
+            // Rail
+            INTERNATIONAL.value(),
+            INTERREGIONAL_RAIL.value(),
+            LOCAL.value(),
+            LONG_DISTANCE.value(),
+            NIGHT_RAIL.value(),
+            AIRPORT_LINK_RAIL.value(),
+            REGIONAL_RAIL.value(),
+            TOURIST_RAILWAY.value(),
+
+            // Air
+            HELICOPTER_SERVICE.value(),
+            DOMESTIC_FLIGHT.value(),
+            INTERNATIONAL_FLIGHT.value(),
+
+            // Water
+            INTERNATIONAL_CAR_FERRY.value(),
+            INTERNATIONAL_PASSENGER_FERRY.value(),
+            LOCAL_CAR_FERRY.value(),
+            LOCAL_PASSENGER_FERRY.value(),
+            NATIONAL_CAR_FERRY.value(),
+            SIGHTSEEING_SERVICE.value(),
+
+            // Taxi
+            CHARTER_TAXI.value(),
+            COMMUNAL_TAXI.value(),
+
+            // All
+            "unknown")
+            + "'";
 
   private static final String MESSAGE = "Illegal TransportSubMode";
 
-  public ValidatedAllowedTransportSubMode() {
-    super(
-      "lines/*[self::Line or self::FlexibleLine]/TransportSubmode[not(. = (" +
-      VALID_TRANSPORT_SUBMODES +
-      "))]",
-      MESSAGE,
-      "TRANSPORT_SUB_MODE"
-    );
-  }
+    public ValidatedAllowedTransportSubMode() {
+        super("lines/*[self::Line or self::FlexibleLine]/TransportSubmode[not(. = (" + VALID_TRANSPORT_SUBMODES + "))]",
+                MESSAGE,
+                "TRANSPORT_SUB_MODE");
+    }
 }
