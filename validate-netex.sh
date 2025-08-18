8c7bc086010c341c3805811e96665e92c0afb54f
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-08-18 09:47:52 +0200

    actually find jar in cli

--- validate-netex.sh
+++ validate-netex.sh
@@ -4,7 +4,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-JAR_FILE=$(find "$SCRIPT_DIR/target" -name "netex-validator-java-*-shaded.jar" -type f -quit 2>/dev/null)
+JAR_FILE=$(find "$SCRIPT_DIR/target" -name "netex-validator-java-*-shaded.jar" -type f -print -quit 2>/dev/null)
 
 if [ ! -f "$JAR_FILE" ]; then
     echo "Shadow jar not found. Build with 'mvn clean package -DskipTests'"
