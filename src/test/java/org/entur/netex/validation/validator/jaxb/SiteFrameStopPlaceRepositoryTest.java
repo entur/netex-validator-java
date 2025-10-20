00b9d138d92fa17ec59769eae097266b72675c49
erlendev <erlend.vige@gmail.com>
2025-10-20 13:44:13 +0200

    Adds unit tests

--- src/test/java/org/entur/netex/validation/validator/jaxb/SiteFrameStopPlaceRepositoryTest.java
+++ src/test/java/org/entur/netex/validation/validator/jaxb/SiteFrameStopPlaceRepositoryTest.java
@@ -2,9 +2,12 @@ package org.entur.netex.validation.validator.jaxb;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import jakarta.xml.bind.JAXBElement;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import javax.xml.namespace.QName;
 import org.entur.netex.index.api.NetexEntitiesIndex;
 import org.entur.netex.index.impl.NetexEntitiesIndexImpl;
 import org.entur.netex.validation.validator.model.QuayCoordinates;
@@ -13,13 +16,7 @@ import org.entur.netex.validation.validator.model.StopPlaceId;
 import org.entur.netex.validation.validator.model.TransportModeAndSubMode;
 import org.entur.netex.validation.validator.model.TransportSubMode;
 import org.junit.jupiter.api.Test;
-import org.rutebanken.netex.model.AllVehicleModesOfTransportEnumeration;
-import org.rutebanken.netex.model.BusSubmodeEnumeration;
-import org.rutebanken.netex.model.LocationStructure;
-import org.rutebanken.netex.model.MultilingualString;
-import org.rutebanken.netex.model.Quay;
-import org.rutebanken.netex.model.SimplePoint_VersionStructure;
-import org.rutebanken.netex.model.StopPlace;
+import org.rutebanken.netex.model.*;
 
 class SiteFrameStopPlaceRepositoryTest {
 
@@ -147,6 +144,68 @@ class SiteFrameStopPlaceRepositoryTest {
     );
 
     assertNull(repository.getStopPlaceNameForQuayId(new QuayId(QUAY_ID)));
+  }
+
+  @Test
+  void testIsParentStop() {
+    NetexEntitiesIndex netexEntitiesIndex = new NetexEntitiesIndexImpl();
+    StopPlace parentStopPlace = new StopPlace();
+    StopPlace nonParentStopPlace = new StopPlace();
+    String nonParentStopPlaceId = "NSR:StopPlace:2";
+    parentStopPlace.setKeyList(
+      new KeyListStructure()
+        .withKeyValue(
+          new KeyValueStructure().withKey("IS_PARENT_STOP_PLACE").withValue("true")
+        )
+    );
+    netexEntitiesIndex.getStopPlaceIndex().put(STOP_PLACE_ID, List.of(parentStopPlace));
+    netexEntitiesIndex
+      .getStopPlaceIndex()
+      .put(nonParentStopPlaceId, List.of(nonParentStopPlace));
+
+    JAXBValidationContext validationContext = createValidationContext(netexEntitiesIndex);
+    SiteFrameStopPlaceRepository repository = new SiteFrameStopPlaceRepository(
+      validationContext
+    );
+
+    assertTrue(repository.isParentStop(new StopPlaceId(STOP_PLACE_ID)));
+    assertFalse(repository.isParentStop(new StopPlaceId(nonParentStopPlaceId)));
+  }
+
+  @Test
+  void testGetQuaysForStopPlaceId() {
+    NetexEntitiesIndex netexEntitiesIndex = new NetexEntitiesIndexImpl();
+    StopPlace stopPlace = new StopPlace();
+    Quay quay1 = new Quay().withId("NSR:Quay:1");
+    Quay quay2 = new Quay().withId("NSR:Quay:2");
+    stopPlace.setQuays(
+      new Quays_RelStructure()
+        .withQuayRefOrQuay(
+          List.of(
+            new JAXBElement<>(
+              new QName("http://www.netex.org.uk/netex", "Quay"),
+              Quay.class,
+              quay1
+            ),
+            new JAXBElement<>(
+              new QName("http://www.netex.org.uk/netex", "Quay"),
+              Quay.class,
+              quay2
+            )
+          )
+        )
+    );
+    netexEntitiesIndex.getStopPlaceIndex().put(STOP_PLACE_ID, List.of(stopPlace));
+
+    JAXBValidationContext validationContext = createValidationContext(netexEntitiesIndex);
+    SiteFrameStopPlaceRepository repository = new SiteFrameStopPlaceRepository(
+      validationContext
+    );
+
+    assertEquals(
+      Set.of(quay1.getId(), quay2.getId()),
+      repository.getQuaysForStopPlaceId(new StopPlaceId(STOP_PLACE_ID))
+    );
   }
 
   @Test
---
+++ src/test/java/org/entur/netex/validation/validator/utils/StopPlaceUtilsTest.java
@@ -0,0 +1,55 @@
+package org.entur.netex.validation.validator.utils;
+
+import jakarta.xml.bind.JAXBElement;
+import java.util.List;
+import java.util.Set;
+import javax.xml.namespace.QName;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.rutebanken.netex.model.*;
+
+class StopPlaceUtilsTest {
+
+  @Test
+  void testIsParentStopPlace() {
+    StopPlace parentStopPlace = new StopPlace();
+    parentStopPlace.setKeyList(
+      new KeyListStructure()
+        .withKeyValue(
+          new KeyValueStructure().withKey("IS_PARENT_STOP_PLACE").withValue("true")
+        )
+    );
+    Assertions.assertTrue(StopPlaceUtils.isParentStopPlace(parentStopPlace));
+
+    StopPlace nonParentStopPlace = new StopPlace();
+    Assertions.assertFalse(StopPlaceUtils.isParentStopPlace(nonParentStopPlace));
+  }
+
+  @Test
+  void testGetQuayIdsForStopPlace() {
+    StopPlace stopPlace = new StopPlace();
+    Quay quay1 = new Quay().withId("NSR:Quay:1");
+    Quay quay2 = new Quay().withId("NSR:Quay:2");
+    stopPlace.setQuays(
+      new Quays_RelStructure()
+        .withQuayRefOrQuay(
+          List.of(
+            new JAXBElement<>(
+              new QName("http://www.netex.org.uk/netex", "Quay"),
+              Quay.class,
+              quay1
+            ),
+            new JAXBElement<>(
+              new QName("http://www.netex.org.uk/netex", "Quay"),
+              Quay.class,
+              quay2
+            )
+          )
+        )
+    );
+    Assertions.assertEquals(
+      Set.of(quay1.getId(), quay2.getId()),
+      StopPlaceUtils.getQuayIdsForStopPlace(stopPlace)
+    );
+  }
+}
